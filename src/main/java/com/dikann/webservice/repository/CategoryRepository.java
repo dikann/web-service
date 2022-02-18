88bd3cb5d210a55e871cfb4ef828e9ebabd11ef6
amir shiati <amirshiati98@gmail.com>
2022-02-18 21:18:40 +0330

    Added category repository

---
+++ src/main/java/com/dikann/webservice/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.dikann.webservice.repository;
+
+import com.dikann.webservice.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
