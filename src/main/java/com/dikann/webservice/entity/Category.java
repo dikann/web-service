27708d19c9f100756457583ee372190cb2f31367
amir shiati <amirshiati98@gmail.com>
2022-02-18 21:17:13 +0330

    Added category entity

---
+++ src/main/java/com/dikann/webservice/entity/Category.java
@@ -0,0 +1,26 @@
+package com.dikann.webservice.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
+@Data
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+    private String name;
+    private String desc;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "created_date")
+    private LocalDateTime createdDate;
+    @Column(name = "modified_date")
+    private LocalDateTime modifiedDate;
+
+}
