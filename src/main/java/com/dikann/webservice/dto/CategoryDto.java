d5719a09ca13dcded62705a1e612cf82cbb0a56a
amir shiati <amirshiati98@gmail.com>
2022-02-19 10:42:08 +0330

    Added category DTO

---
+++ src/main/java/com/dikann/webservice/dto/CategoryDto.java
@@ -0,0 +1,19 @@
+package com.dikann.webservice.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class CategoryDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private long id;
+    private @NotBlank String name;
+    @JsonProperty(value = "description")
+    private String desc;
+    @JsonProperty(value = "image_url")
+    private String imageUrl;
+
+}
