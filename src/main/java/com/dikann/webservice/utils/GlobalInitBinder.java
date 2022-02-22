c4721a800f1d35164109e9473529f78d734d22ef
amir shiati <amirshiati98@gmail.com>
2022-02-22 15:04:44 +0330

    Added global init binder

---
+++ src/main/java/com/dikann/webservice/utils/GlobalInitBinder.java
@@ -0,0 +1,17 @@
+package com.dikann.webservice.utils;
+
+
+import com.dikann.webservice.enums.SortByEnum;
+import com.dikann.webservice.enums.SortDirEnum;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class GlobalInitBinder {
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(SortDirEnum.class, new SortDirEnumConverter());
+        binder.registerCustomEditor(SortByEnum.class, new SortByEnumConvertor());
+    }
+}
--- src/main/java/com/dikann/webservice/utils/SortByEnumConvertor.java
+++ src/main/java/com/dikann/webservice/utils/SortByEnumConvertor.java
@@ -4,6 +4,7 @@ import com.dikann.webservice.enums.SortByEnum;
 
 import java.beans.PropertyEditorSupport;
 
+
 public class SortByEnumConvertor extends PropertyEditorSupport {
     @Override
     public void setAsText(final String text) throws IllegalArgumentException {
