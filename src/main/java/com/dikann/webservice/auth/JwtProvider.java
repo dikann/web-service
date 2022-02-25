f827477c84ebf8f20cb71814279cbcfb7b10cbd0
amir shiati <amirshiati98@gmail.com>
2022-02-25 11:32:04 +0330

    Added user repository

---
+++ src/main/java/com/dikann/webservice/auth/JwtProvider.java
@@ -0,0 +1,2 @@
+package com.dikann.webservice.auth;public class JwtProvider {
+}
---
+++ src/main/java/com/dikann/webservice/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.dikann.webservice.repository;
+
+import com.dikann.webservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String userName);
+}
