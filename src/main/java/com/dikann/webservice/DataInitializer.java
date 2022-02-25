397ed35cd5ee8e4f156c7754f7d2074d7ff5ae2e
amir shiati <amirshiati98@gmail.com>
2022-02-25 17:10:43 +0330

    Added deafult admin config and initializer

--- src/main/java/com/dikann/webservice/DataInitializer.java
+++ src/main/java/com/dikann/webservice/DataInitializer.java
@@ -1,11 +1,19 @@
 package com.dikann.webservice;
 
+import com.dikann.webservice.config.AdminConfig;
 import com.dikann.webservice.entity.Role;
+import com.dikann.webservice.entity.User;
 import com.dikann.webservice.enums.DefaultRoles;
 import com.dikann.webservice.repository.RoleRepository;
+import com.dikann.webservice.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -13,10 +21,36 @@ public class DataInitializer implements CommandLineRunner {
 
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public void run(String... args) throws Exception {
         initRoles();
+        initAdmin();
+    }
+
+    private void initAdmin() {
+        if (userRepository.findByUsername(adminConfig.getUsername()).isPresent())
+            return;
+
+        User admin = new User();
+        admin.setEmail(adminConfig.getEmail());
+        admin.setUsername(adminConfig.getUsername());
+        admin.setPassword(passwordEncoder.encode(adminConfig.getPassword()));
+        List<Role> roles = new ArrayList<>();
+        for (DefaultRoles defaultRoles : DefaultRoles.values())
+            roles.add(roleRepository.findByName(defaultRoles.toString()).get());
+
+        admin.setRoles(roles);
+        admin.setCreatedDate(LocalDateTime.now());
+        admin.setFirstName("default");
+        admin.setLastName("admin");
+        userRepository.save(admin);
     }
 
     private void initRoles() {
---
+++ src/main/java/com/dikann/webservice/config/AdminConfig.java
@@ -0,0 +1,39 @@
+package com.dikann.webservice.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "app.admin")
+public class AdminConfig {
+
+    private String email;
+    private String username;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
