8db32c48182a9db92dd72876aa10b4c3d8d5d8b0
amir shiati <amirshiati98@gmail.com>
2022-02-25 11:50:18 +0330

    Added Jwt auth filter and Jwt provider classes

--- src/main/java/com/dikann/webservice/auth/JwtProvider.java
+++ src/main/java/com/dikann/webservice/auth/JwtProvider.java
@@ -1,2 +1,84 @@
-package com.dikann.webservice.auth;public class JwtProvider {
+package com.dikann.webservice.auth;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
+
+import static com.dikann.webservice.utils.ApplicationConst.AUTHORITIES_KEY;
+import static java.util.stream.Collectors.joining;
+
+@Component
+public class JwtProvider {
+    @Value("${app.jwtSecret}")
+    private String jwtSecret;
+    @Value("${app.jwtExpirationInMs}")
+    private int jwtExpirationInMs;
+
+    private SecretKey secretKey;
+
+
+    @PostConstruct
+    public void init() {
+        var secret = Base64.getEncoder().encodeToString(this.jwtSecret.getBytes());
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(Authentication authentication) {
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Claims claims = Jwts.claims().setSubject(userPrincipal.getUsername());
+        if (!authorities.isEmpty()) {
+            claims.put(AUTHORITIES_KEY, authorities.stream().map(GrantedAuthority::getAuthority).collect(joining(",")));
+        }
+
+//        return Jwts.builder()
+//                .setSubject(userPrincipal.getUsername())
+//                .setIssuedAt(new Date())
+//                .setExpiration(expiryDate)
+//                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+//                .compact();
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(this.secretKey, SignatureAlgorithm.HS256)
+                .compact();
+
+    }
+
+    public String getUserUsernameFromJWT(String token) {
+        Claims claims = Jwts.parserBuilder().setSigningKey(this.secretKey).build().parseClaimsJws(token).getBody();
+//        Claims claims = Jwts.parser()
+//                .setSigningKey(jwtSecret)
+//                .parseClaimsJws(token)
+//                .getBody();
+        return claims.getSubject();
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jws<Claims> claims = Jwts
+                    .parserBuilder().setSigningKey(this.secretKey).build()
+                    .parseClaimsJws(authToken);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+//            System.out.println(e.getCause());
+        }
+        return false;
+    }
 }
--- src/main/java/com/dikann/webservice/utils/ApplicationConst.java
+++ src/main/java/com/dikann/webservice/utils/ApplicationConst.java
@@ -10,6 +10,8 @@ public class ApplicationConst {
     public final static String errorObjectNotFoundCode = "OBJECT_NOT_FOUND";
     public final static String errorObjectNotFoundMessage = "The Object was not found. It was either deleted or doesn't exist!";
 
+    public static final String AUTHORITIES_KEY = "roles";
+
     public static String errorObjectWithNameNotFoundMessage(String name) {
         return String.format("The %s was not found. It was either deleted or doesn't exist!", name);
     }
