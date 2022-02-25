fa2148a38285c30f51e484c213166fc28d5b2153
amir shiati <amirshiati98@gmail.com>
2022-02-25 17:28:19 +0330

    Added auth service

---
+++ src/main/java/com/dikann/webservice/service/AuthService.java
@@ -0,0 +1,42 @@
+package com.dikann.webservice.service;
+
+import com.dikann.webservice.auth.JwtProvider;
+import com.dikann.webservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class AuthService {
+    private final JwtProvider jwtProvider;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+
+    @Autowired
+    public AuthService(JwtProvider jwtProvider, UserRepository userRepository, AuthenticationManager authenticationManager) {
+        this.jwtProvider = jwtProvider;
+        this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+    }
+
+    public Map<Object, Object> signIn(String username, String password) {
+        try {
+            var authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            String token = jwtProvider.generateToken(authentication);
+            Map<Object, Object> model = new HashMap<>();
+            model.put("username", username);
+            model.put("token", token);
+            return model;
+        } catch (AuthenticationException e) {
+            throw new BadCredentialsException("Invalid username or password");
+        }
+    }
+}
