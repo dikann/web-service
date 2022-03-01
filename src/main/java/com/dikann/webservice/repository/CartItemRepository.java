1da9e74e9da1d2e2a287c261a70dad871aee4584
amir shiati <amirshiati98@gmail.com>
2022-03-01 19:12:49 +0330

    Added cart item repository

---
+++ src/main/java/com/dikann/webservice/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.dikann.webservice.repository;
+
+import com.dikann.webservice.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem , Long> {
+}
